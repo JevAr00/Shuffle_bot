695b4c2faf19fb178a933033dec92e60380554fc
Kevin Arias <kevinjosuearias@hotmail.com>
2021-09-22 22:44:48 -0600

    command handler agregado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('../config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles){
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+};
+
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -11,10 +20,15 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping'){
-		await interaction.reply('Pong');
+	if(!command) return;
+
+	try{
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'Oh no! Algo ha pasado tratando de ejecturar ', ephemeral: true});
 	}
 });
 
