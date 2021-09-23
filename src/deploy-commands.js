668c436bf1e914cdbd3cf861c02b8857146387a1
Kevin Arias <kevinjosuearias@hotmail.com>
2021-09-23 11:33:56 -0600

    funcion async para carga de nuevos comandos

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,8 +1,9 @@
-const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9')
 const { clientId, guildId, token } = require('../config.json');
+const fs = require('fs');
 
+//command handler
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles){
@@ -11,6 +12,12 @@ for (const file of commandFiles){
 };
 
 const rest = new REST({version: '9'}).setToken(token);
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(()=> console.log('Comandos registrados correctamente.'))
-    .catch(console.error);
+(async () => {
+    try{
+        console.log('Proceso iniciado. Buscando y actualizando comandos (/).');
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+        console.log('Genial, tienes nuevos comandos (/). Pruebalos!');
+    } catch (error){
+        console.error(error);
+    }
+})();
