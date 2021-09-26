237baf955b9ce86e78e854a5913c8a3b96529bdb
Kevin Arias <kevinjosuearias@hotmail.com>
2021-09-26 14:14:06 -0600

    messageCreate tiene su propio archivo el cual es llamado a index

---
+++ src/events/messageCreate.js
@@ -0,0 +1,17 @@
+module.exports = async (client, message) => {
+    //console.log(message);
+    const prefix = "3";
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+	
+	
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const theCommand = args.shift().toLowerCase();
+    const command = client.commands.get(theCommand);
+
+	try{
+		await command.execute(message);
+	} catch (error) {
+		console.error(error);
+		await message.reply({ content: 'Oh no! Algo ha pasado tratando de ejecutar el comando ', ephemeral: true});
+	}
+}
