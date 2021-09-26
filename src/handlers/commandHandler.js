534829d9c88965d9fe70e92b0ed6a5245df165b8
Kevin Arias <kevinjosuearias@hotmail.com>
2021-09-26 14:11:59 -0600

    correcciones a commandHandler puede ser que ya no lea (/)

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -4,6 +4,6 @@ module.exports = (client) =>{
     const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
     for (const file of commandFiles){
 	    const command = require(`../commands/${file}`);
-	    client.commands.set(command.data.name, command);
+	    client.commands.set(command.name, command);
     };
 };
