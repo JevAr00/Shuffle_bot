5c397ee90acfae145ba90675fc020bb083381303
Kevin Arias <kevinjosuearias@hotmail.com>
2021-09-26 14:12:43 -0600

    correciones de codigo a index.js evento messageCreate esta separado del index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('../config.json');
-const prefix = "-";
-
 
 // Nueva instancia de cliente
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES] });
 
 
-//Command handler
+//Handlers
 client.commands = new Collection();
 client.events = new Collection();
 
 ['commandHandler', 'eventHandler'].forEach(handler => {
 	require(`./handlers/${handler}`)(client);
 })
-
-
 
 /*
 client.on('interactionCreate', async interaction => {
@@ -31,26 +27,6 @@ client.on('interactionCreate', async interaction => {
 		console.error(error);
 		await interaction.reply({ content: 'Oh no! Algo ha pasado tratando de ejecturar ', ephemeral: true});
 	}
-});
-
-
-
-
-client.on('messageCreate', async message => {
-	//console.log(message);
-	if(!message.content.startsWith(prefix) || message.author.bot) return;
-	
-	
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
-
-	try{
-		await command.execute(message);
-	} catch (error) {
-		console.error(error);
-		await message.reply({ content: 'Oh no! Algo ha pasado tratando de ejecturar ', ephemeral: true});
-	}
-
 });
 */
 
