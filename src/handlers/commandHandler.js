f844438bb6496657d6a7342c0d4ab7e0415e2f1f
Kevin Arias <kevinjosuearias@hotmail.com>
2021-10-17 21:49:34 -0600

    actualizadas rutas de readdirsync que daba problemas con las rutas del main

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = (client) =>{
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
     for (const file of commandFiles){
 	    const command = require(`../commands/${file}`);
 	    client.commands.set(command.name, command);
--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = (client) => {
-    const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+    const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
     for (const file of eventFiles){
 	    const event = require(`../events/${file}`);
         const eventName = file.split('.')[0];
