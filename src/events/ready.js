1cbb0f9d473fbf2146704b63a59cd81530ec115c
Kevin Arias <kevinjosuearias@hotmail.com>
2022-04-26 19:19:13 -0600

    refactor: evento ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,10 @@
-module.exports = () => {
-	console.log('Listo');
-};
+const Event = require('../structures/Event');
+
+module.exports = new Event({
+	name: 'ready',
+	once: true,
+
+	execute(client) {
+		console.log(`${client.user.tag} ready to go`);
+	},
+});
