c636c03e72dfd4cb11d57607764ae6ba4fd48de9
Kevin Arias <kevinjosuearias@hotmail.com>
2022-04-27 19:13:04 -0600

    refactor: uso de clase Event

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,15 +1,22 @@
-module.exports = async (client, message) => {
-	const prefix = '!';
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+const Event = require('../structures/Event');
 
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
-	const commandList = client.commands.get(command) || client.commands.find(c => c.aliases && c.aliases.includes(command));
+module.exports = new Event({
+	name: 'messageCreate',
+	once: false,
 
-	try {
-		await commandList.execute(client, message, command, args);
-	}
-	catch (error) {
-		await message.reply({ content: 'Oh no! Algo ha pasado tratando de ejecutar el comando ', ephemeral: true });
-	}
-};
+	async execute(client, message) {
+		const prefix = '!';
+		if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+		const args = message.content.slice(prefix.length).split(/ +/);
+		const command = args.shift().toLowerCase();
+		const commandList = client.commands.get(command) || client.commands.find(c => c.aliases && c.aliases.includes(command));
+
+		try {
+			await commandList.execute(client, message, command, args);
+		}
+		catch (error) {
+			await message.reply({ content: 'Oh no! Algo ha pasado tratando de ejecutar el comando ', ephemeral: true });
+		}
+	},
+});
