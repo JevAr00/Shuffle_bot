af2d66e76da39f96a51d72f2866319488ea7c96e
Kevin Arias <kevinjosuearias@hotmail.com>
2022-04-30 01:35:31 -0600

    add: nueva version del comando play

---
+++ src/commands/normal/nplay.js
@@ -0,0 +1,48 @@
+const Command = require('../../structures/Command');
+const {
+	playerStatus,
+	joinVoice,
+	getPlayer,
+	createPlayerResource,
+	searchSong,
+} = require('../../helpers/player');
+
+module.exports = new Command({
+	name: 'nplay',
+	aliases: 'p',
+
+	async execute(client, message, command, args) {
+		const voiceChannel = message.member.voice.channel;
+		if (!voiceChannel) return message.reply('Parece que no estas dentro de un canal de voz al que pueda unirme');
+
+		const player = getPlayer();
+		const currentSong = player.state.resource.metadata.title;
+		const isPaused = player.state.status === playerStatus.Paused;
+
+		if (!args.length) {
+			if (isPaused) {
+				player.unpause();
+				return message.channel.send(`${currentSong} esta sonando`);
+			}
+			else {
+				return message.channel.send('No he recibido un nombre o URL de una cancion');
+			}
+		}
+
+		const connection = joinVoice(voiceChannel, message.guild);
+
+		const song = await searchSong(args);
+		const resource = createPlayerResource(song);
+
+		player.play(resource);
+		connection.subscribe(player);
+
+		player.once(playerStatus.Playing, () => {
+			message.channel.send(`${song.title} esta sonando`);
+		});
+
+		player.on(playerStatus.Idle, () => {
+			connection.destroy();
+		});
+	},
+});
