c3ddd9c408d9c08fc9f9103818a5c75482c126f9
Kevin Arias <kevinjosuearias@hotmail.com>
2022-04-29 18:32:25 -0600

    refactor: ruta de carpeta de comandos por evento messageCreate

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 module.exports = (client) => {
-	const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+	const commandFiles = fs.readdirSync('src/commands/normal/').filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
-		const command = require(`../commands/${file}`);
+		const command = require(`../commands/normal/${file}`);
 		client.commands.set(command.name, command);
 	}
 };
